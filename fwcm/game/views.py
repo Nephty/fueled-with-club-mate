3009e771e28ec9ad6d32e24b704082af9c5b76af
Nephty <arnaud.moreau@outlook.com>
2024-11-23 23:46:21 +0100

    added dynamic website

--- fwcm/game/views.py
+++ fwcm/game/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 import random
 
-pages = [
+static_pages = [
     "amazon", "odoo", "linkedin", "youtube", "wikipedia", "chatgpt", "reddit", "google"
 ]
+
 working_pages = [
-    "odoo", "linkedin", "youtube", "wikipedia", "chatgpt", "reddit",
+    "odoo", "linkedin", "youtube", "wikipedia", "reddit", "yahoo", "pinterest", "discord", "twitch", "spotify"
 ]
 
 def dynamic_game_view(request):
@@ -15,7 +16,7 @@ def dynamic_game_view(request):
     return render(request, 'game.html', {'html': html, 'answer': page})
 
 def static_game_view(request):
-    page = random.choice(pages)
+    page = random.choice(static_pages)
     print(page)
     html = f'https://harbiinger.xyz/static_websites/{page}_rendered.html'
     return render(request, 'game.html', {'html': html, 'answer': page})
