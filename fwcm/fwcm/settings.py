010887979cae144c3f860474016b4c6c5770e30e
Arnaud Moreau <72945143+Nephty@users.noreply.github.com>
2024-11-23 14:30:52 +0000

    make font static work

--- fwcm/fwcm/settings.py
+++ fwcm/fwcm/settings.py
@@ -175,7 +175,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 LOGIN_URL = '/login/'
 
