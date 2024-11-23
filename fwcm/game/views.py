1cb10223d69566622756b3fa53822d200232738d
Nephty <arnaud.moreau@outlook.com>
2024-11-23 22:45:59 +0100

    switch to harbiinger.xyz

--- fwcm/game/views.py
+++ fwcm/game/views.py
@@ -3,12 +3,12 @@ import random
 
 def dynamic_game_view(request):
     html = 'http://127.0.0.1:8000/serve'
-    return render(request, 'game.html', {'html':html})
+    return render(request, 'game.html', {'html': html})
 
 def static_game_view(request):
     pages = [
             "amazon", "odoo", "linkedin", "youtube", "wikipedia", "chatgpt", "reddit", "google"
     ]
     page = random.choice(pages)
-    html = f'http://127.0.0.1:8001/{page}_rendered.html'
-    return render(request, 'game.html', {'html':html})
+    html = f'https://harbiinger.xyz/static_websites/{page}_rendered.html'
+    return render(request, 'game.html', {'html': html})
