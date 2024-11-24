ab253bcca08062b4cb79cb1f30157000a6463a10
Nephty <arnaud.moreau@outlook.com>
2024-11-24 12:13:19 +0100

    scripted game

--- fwcm/game/views.py
+++ fwcm/game/views.py
@@ -10,25 +10,26 @@ working_pages = [
     "odoo", "linkedin", "youtube", "wikipedia", "reddit", "yahoo", "pinterest", "discord", "twitch", "spotify"
 ]
 
+scripted_pages = ["youtube", "odoo", "wikipedia", "linkedin", "chatgpt"]
+
 def dynamic_game_view(request):
     page = random.choice(working_pages)
     print(page)
     html = f'http://127.0.0.1:8000/serve?page={page}'
-    return render(request, 'game.html', {'html': html, 'answer': page})
+    return render(request, 'game.html', {'html': html, 'answer': page, 'scripted': False})
 
 def static_game_view(request):
     page = random.choice(static_pages)
     print(page)
     html = f'https://harbiinger.xyz/static_websites/{page}_rendered.html'
-    return render(request, 'game.html', {'html': html, 'answer': page})
+    return render(request, 'game.html', {'html': html, 'answer': page, 'scripted': False})
 
 def scripted_game_view(request):
     rem = request.COOKIES.get("remaining")
-    pages = ["youtube", "odoo", "wikipedia", "linkedin", "chatpgpt"]
-    page = pages.pop(int(rem)-1)
+    page = scripted_pages[int(rem)-1]
     html = f'https://harbiinger.xyz/static_websites/{page}_rendered.html'
 
-    return render(request, 'game.html', {'html':html, 'answer':page})
+    return render(request, 'game.html', {'html': html, 'answer': page, 'scripted': True})
 
 def end_game_view(request):
     score = request.COOKIES.get("score")
