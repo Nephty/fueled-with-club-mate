4810519f439e322ec44aa7398269cb5e2bc7ab68
Ugo Proietti <account.pr@protonmail.com>
2024-11-24 04:32:02 +0100

    Update views.py

--- fwcm/game/views.py
+++ fwcm/game/views.py
@@ -9,6 +9,7 @@ working_pages = [
     "odoo", "linkedin", "youtube", "wikipedia", "reddit", "yahoo", "pinterest", "discord", "twitch", "spotify"
 ]
 
+@csrf_exempt
 def dynamic_game_view(request):
     page = random.choice(working_pages)
     print(page)
